d84df2c029e87e8c431b7dd7833d7c784cb1e887
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-05-22 23:10:02 +0530

    Disable auto generating specs

--- config/application.rb
+++ config/application.rb
@@ -22,5 +22,13 @@ module BuyThingsShop
 
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
+
+    config.generators do |g|
+        g.view_specs false
+        g.helper_specs false
+        g.controller_specs false
+        g.request_specs false
+        g.routing_specs false
+    end
   end
 end
