941acbf24b2d7cbdd39724510bc08dcd974dddeb
Chanaka Sandaruwan <chanakasan@gmail.com>
2015-05-22 23:57:14 +0530

    Generate product scaffolds

--- db/schema.rb
+++ db/schema.rb
@@ -11,6 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 0) do
+ActiveRecord::Schema.define(version: 20150522182606) do
+
+  create_table "products", force: :cascade do |t|
+    t.string   "name"
+    t.decimal  "price"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
 end
